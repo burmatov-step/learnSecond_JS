5d6fbf600f9f82d420b6e67dcf148d3dad9df3ac
burmatov-step <stepaburmatov@mail.ru>
2020-08-26 11:12:54 +0300

    end lesson

--- main.js
+++ main.js
@@ -1,39 +1,24 @@
 "use strict";
 
-function saleSum(sumAll, products, promo ) {
-  let sale = 0;
-  let sumEnd;
-  if(products >= 5 && products < 10){
-    sale += sumAll*0.05
-  } else if(products >= 10){
-    sale += sumAll*0.1
+
+
+
+class First {
+  hello(){
+    console.log('Привет, я метод родителя');
   }
+}
 
-  if(sumAll > 100000){
-    sale += (sumAll - 100000)*0.2
+class Second extends First{
+  hello(){
+    super.hello();
+    console.log('А я наследуемый метод')
   }
-
-  if(promo === '15' && sumAll > 25000){
-    sale += sumAll*0.15
-  }
-
-  if(promo === '100' && sumAll > 100){
-    sale += 100
-  } else if(promo === '100' && sumAll <= 100){
-    sumEnd = 0;
-    return sumEnd;
-  }
-
-
-  if(sale === 0){
-    return
-  } else{
-    sumEnd = sumAll - sale;
-    return sumEnd;
-  }
-
 }
 
 
-console.log(saleSum(150000, 10, '100'));
+const first = new First();
+const second = new Second();
 
+first.hello();
+second.hello();
